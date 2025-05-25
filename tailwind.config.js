3829dd5ddb4f8e19dc2c4553bc9416b661fc7537
Amy C <git@amyy.me>
2025-05-25 19:20:47 -0400

    More ttree utilization, colored markers

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,12 @@ module.exports = {
                 'accent': '#bb8aff',
                 'bg-primary': '#25222a',
                 'bg-secondary': '#282828',
-
+                'language-python': '#3572a5',
+                'language-rust': '#dea584',
+                'language-go': '#00add8',
+                'language-typescript': '#3178c6',
+                'language-csharp': '#178600',
+                'language-cplusplus': '#f34b7d'
             }
         },
     },
